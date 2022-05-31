436a845a96b08875f2fdabecc4819383a4b36b5e
maoxiaoxing <930649054@qq.com>
2022-05-31 08:47:08 +0800

    feat: MyReadonly2

--- src/type-challenges/00008-medium-readonly-2/template.ts
+++ src/type-challenges/00008-medium-readonly-2/template.ts
@@ -3,5 +3,18 @@
   然后排出 K 在 T 中的所有的元素
   两者取交集
 */
-type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & Omit<T, K>
+// export type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & Omit<T, K>
+
+export type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+type MyExclude<T, K> = T extends K ? never : T
+
+type MyOmit<T, K> = {
+  [P in MyExclude<keyof T, K>]: T[P]
+}
+
+export type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<MyPick<T, K>> & MyOmit<T, K>
+
 
