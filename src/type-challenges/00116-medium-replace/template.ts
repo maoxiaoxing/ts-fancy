a9019916d1f41fdc9c1d97b91f8de49973c624ce
maoxiaoxing <930649054@qq.com>
2022-06-07 08:04:21 +0800

    Replace

--- src/type-challenges/00116-medium-replace/template.ts
+++ src/type-challenges/00116-medium-replace/template.ts
@@ -1,4 +1,4 @@
 type Replace<S extends string, From extends string, To extends string> = 
   From extends '' 
-    ? S : S extends `${infer A}${infer From}${infer B}`
-        ? `${A}${To}${B}` : S
+    ? S : S extends `${infer Front}${infer From}${infer Behind}`
+        ? `${Front}${To}${Behind}` : S
