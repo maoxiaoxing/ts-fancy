523cd1c8ca41bd4b09420c512226ef8727d474ec
maoxiaoxing <930649054@qq.com>
2022-06-08 08:18:34 +0800

    MyAwaited

--- src/type-challenges/00119-medium-replaceall/template.ts
+++ src/type-challenges/00119-medium-replaceall/template.ts
@@ -2,6 +2,6 @@ type ReplaceAll<S extends string, From extends string, To extends string> =
   S extends ''
     ? S : S extends `${infer Front}${From}${infer Behind}`
       ? From extends '' 
-        ? S : `${Front}${To}${ReplaceAll<Behind, From, To>}` : S
+      ? S :`${Front}${To}${ReplaceAll<Behind, From, To>}` : S
 
 
---
+++ src/type-challenges/00189-easy-awaited/template.ts
@@ -0,0 +1,5 @@
+// type MyAwaited = any
+type MyReAwaited<T> = T extends Promise<infer U> ? MyReAwaited<U> : T
+type MyAwaited<T extends Promise<unknown>> = MyReAwaited<T>
+
+type a  = Awaited<Promise<1>>
