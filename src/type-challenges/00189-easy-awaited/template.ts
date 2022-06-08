df6e9491ec66dcdc5c8355b9ab8e69103b0e96c0
maoxiaoxing <930649054@qq.com>
2022-06-08 08:28:32 +0800

    AppendArgument

--- src/type-challenges/00189-easy-awaited/template.ts
+++ src/type-challenges/00189-easy-awaited/template.ts
@@ -2,4 +2,4 @@
 type MyReAwaited<T> = T extends Promise<infer U> ? MyReAwaited<U> : T
 type MyAwaited<T extends Promise<unknown>> = MyReAwaited<T>
 
-type a  = Awaited<Promise<1>>
+// type a  = Awaited<Promise<1>>
