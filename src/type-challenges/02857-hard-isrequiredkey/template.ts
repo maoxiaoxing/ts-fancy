4d2b7967b61366136054d6095637e2e5afcf6d82
maoxiaoxing <930649054@qq.com>
2022-06-20 08:27:54 +0800

    IsRequiredKey

--- src/type-challenges/02857-hard-isrequiredkey/template.ts
+++ src/type-challenges/02857-hard-isrequiredkey/template.ts
@@ -2,5 +2,5 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 // export type IsRequiredKey<T, K extends keyof T> = Equal<{[P in K]: T[P]}, {[P in K]-?: T[P]}>
 
-export type IsRequiredKey<T, K extends keyof T> = Record<K, T[K]> extends T ? true : false
+export type IsRequiredKey<T, K extends keyof T> = T extends Record<K, T[K]> ? true : false
 
