9b752e42efb25a64d4fa6809015a868a3d91b1de
maoxiaoxing <930649054@qq.com>
2022-06-30 21:20:11 +0800

    CamelCase

--- src/type-challenges/00114-hard-camelcase/template.ts
+++ src/type-challenges/00114-hard-camelcase/template.ts
@@ -1,9 +1,15 @@
-type CamelCase<S extends string, R extends string = ''> =
-  S extends `${infer First}_${infer Rest}`
-  ? R extends ''
-    ? CamelCase<Lowercase<Rest>, `${R}${Lowercase<First>}`>
-    : CamelCase<Lowercase<Rest>, `${R}${Capitalize<First>}`>
-  : R extends ''
-    ? Lowercase<S>
-  : `${R}${Capitalize<S>}`
+// type CamelCase<S extends string, R extends string = ''> =
+//   S extends `${infer First}_${infer Rest}`
+//   ? R extends ''
+//     ? CamelCase<Lowercase<Rest>, `${R}${Lowercase<First>}`>
+//     : CamelCase<Lowercase<Rest>, `${R}${Capitalize<First>}`>
+//   : R extends ''
+//     ? Lowercase<S>
+//   : `${R}${Capitalize<S>}`
+
+type Help<S extends string> = S extends `${infer L}_${infer R}`
+  ? `${L}${Help<Capitalize<R>>}`
+  : S;
+
+export type CamelCase<S extends string> = Help<Lowercase<S>>;
 
--- src/type-challenges/00114-hard-camelcase/test-cases.ts
+++ src/type-challenges/00114-hard-camelcase/test-cases.ts
@@ -1,4 +1,5 @@
 import type { Equal, Expect } from '@type-challenges/utils'
+import { CamelCase } from './template'
 
 type cases = [
   Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
